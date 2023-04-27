18dc1c258650244f8d8755c49bd792e898a07dfc
Jonathan Torres <jonathan.torresmald@gmail.com>
2023-04-28 01:20:23 +0200

    finished

--- src/context/ClimaProvider.jsx
+++ src/context/ClimaProvider.jsx
@@ -23,7 +23,7 @@ const ClimaProvider = ({ children }) => {
         try {
             const appId = import.meta.env.VITE_API;
             const { ciudad, pais } = busqueda;
-            const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=3&appid=${appId}`;
+            const url = `https://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=3&appid=${appId}`;
             const { data } = await axios.get(url)
             const { lat, lon } = data[0];
             const peticion = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`;
